86639c4def16efb72f3f4257276cc65ece09e80c
celebican21 <burhan.celebi.2112@gmail.com>
2020-11-23 17:45:15 +0300

    Interface functions are fixed

--- src/Factory/AMQPMessage.php
+++ src/Factory/AMQPMessage.php
@@ -8,11 +8,11 @@ interface AMQPMessage
 {
     public function channel();
     
-    public function queueDeclare(bool $passive, bool $durable, string $exclusive, bool $auto_delete);
+    public function queueDeclare(array $queue_declare = []);
 
     public function queueBind();
     
-    public function exchangeDeclare(int $exchange_type, bool $passive, bool $durable, bool $auto_delete);
+    public function exchangeDeclare(array $exchange_declare = []);
 
     // public function messageBody($closure, $function);
 }
